ba0034210609d6db2acaea0b83ffa94d933a58ef
Mohammad Ali Gholami <mamaddl@live.com>
2022-04-28 14:57:48 +0430

    Shootout Mental Break base class added

---
+++ Sources/N.GuardTowers/GuardTowers/NGT_MentalBreaks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.AI;
+using RimWorld;
+using Verse;
+
+namespace NGT
+{
+    public class MB_Shootout : MentalState
+    {
+		public override bool ForceHostileTo(Thing t)
+		{
+			return true;
+		}
+		public override bool ForceHostileTo(Faction f)
+		{
+			return true;
+		}
+		public override RandomSocialMode SocialModeMax()
+		{
+			return RandomSocialMode.Off;
+		}
+        public override void PreStart()
+        {
+            base.PreStart();
+        }
+    }
+}
