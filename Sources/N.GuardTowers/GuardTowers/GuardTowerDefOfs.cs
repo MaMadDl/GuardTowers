84d465b828a47688be996e95c3e86da75ab6e0d5
Mohammad Ali Gholami <mamaddl@live.com>
2022-04-29 17:37:24 +0430

    DefOf MentalState Added

---
+++ Sources/N.GuardTowers/GuardTowers/GuardTowerDefOfs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace NGT
+{
+    [DefOf]
+    public static class GuardTowerDefOf
+    {
+        public static MentalStateDef NGT_towerShootout;
+
+    }
+}
