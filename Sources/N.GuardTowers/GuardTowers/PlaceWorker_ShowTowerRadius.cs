01f133624f0198b301c63cfdb512c0a171ba3ede
Mohammad Ali Gholami <mamaddl@live.com>
2022-08-08 04:36:47 +0430

    Adjacent PlaceWorker

--- Sources/N.GuardTowers/GuardTowers/PlaceWorker_ShowTowerRadius.cs
+++ Sources/N.GuardTowers/GuardTowers/PlaceWorker_ShowTowerRadius.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using Verse;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NGT
 {
@@ -14,13 +16,42 @@ namespace NGT
             {
                 return;
             }
+			if (thing.def.IsBlueprint || thing.def.IsFrame)
+            {
+				List<IntVec3> t= GenAdj.OccupiedRect(center, rot, def.Size).ExpandedBy(6).ToList();
+				GenDraw.DrawFieldEdges(t);
+				return;
+            }
             ThingOwner<Pawn> colList = ((BaseGuardTower)thing).GetInner();
             int i = 0;
             foreach (var col in colList.InnerListForReading) {
-                i= (i+2)%8;
-               
+                i= (i+2)%8;  
                 GenDraw.DrawCircleOutline(center.ToVector3(),col.equipment.Primary.def.Verbs[0].range,(SimpleColor)i);
             }
         }
-    }
+	}
+
+	public class PlaceWorker_NeverAdjacentTower : PlaceWorker
+	{
+		public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 center, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			foreach (IntVec3 c in GenAdj.OccupiedRect(center, rot, def.Size).ExpandedBy(6))
+			{
+				if (c.InBounds(map))
+				{
+					List<Thing> list = map.thingGrid.ThingsListAt(c);
+					for (int i = 0; i < list.Count; i++)
+					{
+						Thing thing2 = list[i];
+						ThingDef thingDef;
+						if (thing2 != thingToIgnore && ((thing2.def.category == ThingCategory.Building && thing2.def == def) || ((thing2.def.IsBlueprint || thing2.def.IsFrame) && (thingDef = (thing2.def.entityDefToBuild as ThingDef)) != null && thingDef == def)))
+						{
+							return "CannotPlaceAdjacentTower".Translate();
+						}
+					}
+				}
+			}
+			return true;
+		}
+	}
 }
